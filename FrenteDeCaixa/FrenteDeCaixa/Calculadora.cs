b70414d808c2231441ad5261d13be9c51e9dd362
walefismael <48078620+walefismael@users.noreply.github.com>
2019-02-28 01:46:50 -0300

    Add files via upload

---
+++ FrenteDeCaixa/FrenteDeCaixa/Calculadora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrenteDeCaixa
+{
+    public class Calculadora
+    {
+        public decimal Valor;
+        public int Qtd;
+
+        public decimal Total()
+        {
+            return Qtd * 1.99m;
+        }
+        public decimal Troco()
+        {
+            return Valor - Total();
+        }
+
+    }
+}
---
+++ FrenteDeCaixa/FrenteDeCaixa/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrenteDeCaixa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int op;
+
+
+            Console.WriteLine("======MERCADIM 1,99======\n\n");
+            Console.WriteLine("0 - SAIR");
+            Console.WriteLine("1 - VENDER\n");
+            Console.Write("Escolha a opção desejada: ");
+            op = Convert.ToInt16(Console.ReadLine());
+            do
+            {
+                switch (op)
+                {
+                    case 0:
+
+                        break;
+
+                    case 1:
+                        Calculadora calc = new Calculadora();
+
+                        Console.Clear();
+
+                        Console.WriteLine("=========VENDAS========= \n");
+                        Console.Write("Quantidade de prod: ");
+                        calc.Qtd = Convert.ToInt16(Console.ReadLine());
+
+                        decimal total = calc.Total();
+                        Console.WriteLine("Total da compra: " + total);
+                        Console.WriteLine("");
+                        Console.Write("Dinheiro do cliente: R$ ");
+                        calc.Valor = Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine();
+                        decimal troco = calc.Troco();
+                        Console.Write("Troco: " + troco);
+
+                        Console.WriteLine("\n\nDeseja fazer mais uma venda? S/N");
+                        char resposta = Convert.ToChar(Console.ReadLine());
+
+                        if (resposta == 's' || resposta == 'S')
+                        {
+
+                        }
+                        else
+                        {
+                            op = 0;
+                        }
+
+
+
+                        break;
+                }
+
+            } while (op != 0);
+        }
+
+    }
+}
+
